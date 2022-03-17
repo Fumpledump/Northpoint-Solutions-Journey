30e5ed6d43fd23c9c13522b4dfe6c57a6ca8f8f9
Mr.Fumpledump <Fumpledump@gmail.com>
2022-03-17 05:57:37 -0400

    Experimenting with Floating Text Above Player
    Experimenting with Floating Text that will be used as the issue to be solved in the solution journey for the game.
    Includes:
    -Floating Text
    -Size Rescaling Depending on the Text
    -Code for setting the Text
    
    Plans:
    -Add Menu Feature to edit this set text before the game starts.
    
    Current Bugs:
    -Flipped when Player goes right

---
+++ Assets/Scripts/IssueText.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class IssueText : MonoBehaviour
+{
+    public SpriteRenderer backgroundSpriteRenderer;
+    public TextMeshPro textMeshPro;
+
+    private void Start()
+    {
+        Setup("Hello World!");
+    }
+    private void Setup(string text)
+    {
+        textMeshPro.SetText(text);
+        textMeshPro.ForceMeshUpdate();
+        Vector2 textSize = textMeshPro.GetRenderedValues(false);
+
+        Vector2 padding = new Vector2(1f, 0.5f);
+        backgroundSpriteRenderer.size = textSize + padding;
+
+    }
+}
